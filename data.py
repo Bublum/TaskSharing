474d8ac1c7f92cc1a501e6b50052d89f6da8d01d
anish <anishsatalkar@gmail.com>
2018-09-06 17:14:40 +0530

    data server accepts coordinator connection requests , sends required files

--- data.py
+++ data.py
@@ -1,24 +1,10 @@
 import json
 import socket
 import netifaces as ni
-import os
-import cv2
-import pickle
 
 BUFFER_SIZE = 1024
-
-
-def send_sample_code(path, client_sock):
-    size = os.path.getsize(path)
-    client_sock.send(create_sample_code_message('sample', '0', '100', number_of_bytes=size, chunk_size=BUFFER_SIZE))
-    file = open(path, 'r')
-
-    data = file.read(BUFFER_SIZE)
-    while file:
-        client_sock.send(data)
-        data = file.read(BUFFER_SIZE)
-    file.close()
-    client_sock.close()
+coordinator_ip = '127.0.0.1'
+coordinator_port = 5039
 
 
 class DataServer:
@@ -29,31 +15,28 @@ class DataServer:
         self.port = port
 
 
-coordinator_ip = '192.168.0.109'
-coordinator_port = 5600
+def create_init_message(self_hostname, self_ip, self_port):
+    return json.dumps({
+        'message_type': 'server_metadata',
+        'data_server_host_name': self_hostname,
+        'data_server_ip': self_ip,
+        'data_server_port': self_port
+    }).encode('UTF-8')
 
 
-def create_message(self_hostname, self_ip, self_port):
-    return {
-        'type': 'data',
-        'data_host_name': self_hostname,
-        'data_ip': self_ip,
-        'data_port': self_port
-    }
-
-
-def create_sample_code_message(type, start_frame, end_frame, number_of_bytes, chunk_size):
-    return {
+def create_file_send_message(type, start_frame, end_frame, number_of_bytes, chunk_size):
+    return json.dumps({
         'type': type,
         'start_frame': start_frame,
         'end_frame': end_frame,
         'num_of_bytes': number_of_bytes,
         'chunk_size': chunk_size,
-    }
+    }).encode('UTF-8')
 
 
 def init_self(port):
-    self_ip = ni.ifaddresses('wlp2s0')[ni.AF_INET][0]['addr']
+    print(ni.interfaces())
+    self_ip = ni.ifaddresses('wlp3s0')[ni.AF_INET][0]['addr']
     self_hostname = socket.gethostname()
     data_server = DataServer(self_ip, self_hostname, port)
     return data_server
@@ -61,26 +44,49 @@ def init_self(port):
 
 def init(data_server):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((data_server.ip, data_server.port))
-    s.connect((coordinator_ip,coordinator_port))
-    init_message = json.dumps(create_message(data_server.hostname, data_server.ip, data_server.port))
-    s.send(init_message.encode('UTF-8'))
-    return s
+    s.connect((coordinator_ip, coordinator_port))
+    init_message = create_init_message(data_server.hostname, data_server.ip, data_server.port)
+    s.send(init_message)
+    s.close()
 
 
 message_types = {
     'GET_DATA': ''
 }
 
+
+def send_file(path, client_sock, type):
+    print('Sending file')
+    try:
+        file = open(path, 'r')
+        data = file.read(BUFFER_SIZE)
+        client_sock.send(
+            create_file_send_message(type, '0', '100', number_of_bytes=len(data), chunk_size=BUFFER_SIZE))
+        while data:
+            client_sock.send(data)
+            data = file.read(BUFFER_SIZE)
+        file.close()
+        client_sock.close()
+    except IOError:
+        client_sock.send(json.dumps({'error': str(IOError.filename)}).encode('UTF-8'))
+
+
 if __name__ == '__main__':
     data_server = init_self(5037)
-    socket_obj = init(data_server)
+    init(data_server)
+    socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_obj.bind((data_server.ip, data_server.port))
     socket_obj.listen(1)
     while 1:
         client_sock, address = socket_obj.accept()
         if client_sock:
+            print('Connection with coordinator established.')
             data = client_sock.recv(BUFFER_SIZE)
             data_json = json.loads(data)
             print(data_json)
-            if data_json['MESSAGE_TYPE'] == 'SAMPLE_CODE':
-                send_sample_code('test_text_file.txt', client_sock)
+            if data_json['message_type'] == 'sample_code':
+                send_file('test_text_file.txt', client_sock, type='sample_code')
+            elif data_json['message_type'] == 'client_code':
+                send_file('client_code.py', client_sock, type='client_code')
+            elif data_json['message_type'] == 'server_code':
+                send_file('server_code.py', client_sock, type='server_code')
